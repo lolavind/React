43e0d6063d7fd13cd6b76f8e73734fb2a6f430ee
Lovisa Landberg <lovisa@landberg.org>
2025-07-23 22:01:35 +0200

    Kod för Like.jsx

---
+++ my-app/src/Like.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+function LikeButton() {
+	const [liked, setLiked] = useState(false);
+	const [likes, setLikes] = useState(0);
+
+	const toggleLike = () => {
+		if (liked) {
+			setLikes(likes - 1);
+		} else {
+			setLikes(likes + 1);
+		}
+		setLiked(!liked);
+	};
+
+	return (
+		<button onClick={toggleLike}>
+			{liked ? '❤️ Gillad' : '🤍 Gilla'} ({likes})
+		</button>
+	);
+}
+
+export default LikeButton;
